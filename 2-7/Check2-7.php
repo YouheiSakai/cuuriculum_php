69e37f6bce83cebd34e67d5a7ea04efb655378de
youhei-S <105723355+YouheiSakai@users.noreply.github.com>
2022-06-20 22:48:27 +0900

    Update Check2-7.php

--- 2-7/Check2-7.php
+++ 2-7/Check2-7.php
@@ -1,8 +1,8 @@
 <?php
-$fruits1 = ["red" => "赤","blue" => "青", "green" => "緑"];
-$fruits2 = ["red" => "赤","blue" => "青", "green" => "緑", "yellow" => "黄色"];
+$color1= ["red" => "赤","blue" => "青", "green" => "緑"];
+$color2 = ["red" => "赤","blue" => "青", "green" => "緑", "yellow" => "黄色"];
 
-var_dump($fruits1);
+var_dump($color1);
 echo '<br/>';
-var_dump($fruits2);
+var_dump($color2);
 ?>
