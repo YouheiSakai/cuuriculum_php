5462ce3474eba8b499314ba98ad28b294393cb75
youhei-S <105723355+YouheiSakai@users.noreply.github.com>
2022-06-20 22:44:58 +0900

    2-7

---
+++ 2-7/Check2-7.php
@@ -0,0 +1,8 @@
+<?php
+$fruits1 = ["red" => "赤","blue" => "青", "green" => "緑"];
+$fruits2 = ["red" => "赤","blue" => "青", "green" => "緑", "yellow" => "黄色"];
+
+var_dump($fruits1);
+echo '<br/>';
+var_dump($fruits2);
+?>
