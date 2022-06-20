4638cdb8bee831819d5befced5a61e561778dc65
YouheiSakai <105723355+YouheiSakai@users.noreply.github.com>
2022-06-20 22:37:57 +0900

    Delete 2-7 directory

--- 2-7/Check2-7.php
+++
@@ -1,8 +0,0 @@
-<?php
-$fruits1 = ["red" => "赤","blue" => "青", "green" => "緑"];
-$fruits2 = ["red" => "赤","blue" => "青", "green" => "緑", "yellow" => "黄色"];
-
-var_dump($fruits1);
-echo '<br/>';
-var_dump($fruits2);
-?>
