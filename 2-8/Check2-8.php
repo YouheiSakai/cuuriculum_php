e2a8a98a2ecdb0b02ea0b7da5a2549ae2220e893
youhei-S <105723355+YouheiSakai@users.noreply.github.com>
2022-07-05 21:26:30 +0900

    Update Check2-8.php

--- 2-8/Check2-8.php
+++ 2-8/Check2-8.php
@@ -1,9 +1,7 @@
 <?php
-$fruits = ["apple"=>"といったらりんご","orange"=>"といったらみかん","peach"=>"といったらもも"];
+$fruits = ["apple"=>"りんご","orange"=>"みかん","peach"=>"もも"];
 
 foreach($fruits as $key=>$value){
-    echo $key;
-    echo $value;
-    echo '<br/>';
+    echo $key. "といったら". $value. '<br/>';
 }
 ?>
