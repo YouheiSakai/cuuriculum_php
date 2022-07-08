f69733086d002bf60a6f47dab2ca82de50c05cfa
youhei-S <105723355+YouheiSakai@users.noreply.github.com>
2022-07-08 22:09:31 +0900

    2-11

---
+++ 2-11/Check2-11_form.php
@@ -0,0 +1,9 @@
+<form action="Check2-11_result.php" method="post">
+    名前:<input type="text" name="name" />
+    <br>
+    メールアドレス:<input type="email" name="mail" />
+    <br>
+    パスワード:<input type="password" name="password" />
+    <br>
+    <input type="submit" value="送信" />
+</form>
---
+++ 2-11/Check2-11_result.php
@@ -0,0 +1,8 @@
+<?php
+$name = $_POST['name'];
+$mail = $_POST['mail'];
+$password = $_POST['password'];
+?>
+<p>私の名前は、<?php echo $name; ?></p>
+<p>私のパスワードは、<?php echo $password; ?></p>
+<p>私のメールアドレスは、<?php echo $mail; ?></p>
